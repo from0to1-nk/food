be756c1ddb115a14aea584a60fcd8788a2e56b93
Nadezhda <skykerc@gmail.com>
2022-04-21 17:57:14 +0300

    timer old data

--- js/main.js
+++ js/main.js
@@ -34,14 +34,22 @@ window.addEventListener('DOMContentLoaded', () => {
 })
 ////////////////////timer///////////////////////
 
-const deadline = '2022-05-30';
+const deadline = '2022-07-30';
 
 function calcTime(endtime) {
+    let days, hours, minutes, seconds;
     const t = Date.parse(endtime) - Date.parse(new Date());
-    let days = Math.floor(t / (1000 * 60 * 60 * 24)),
-        hours = Math.floor(t / (1000 * 60 / 60) % 24),
-        minutes = Math.floor(t / (1000 / 60) % 60),
-        seconds = Math.floor(t / (1000) % 60)
+    if (t <= 0) {
+        days = 0;
+        hours = 0;
+        minutes = 0;
+        seconds = 0;
+    } else {
+        days = Math.floor(t / (1000 * 60 * 60 * 24)),
+            hours = Math.floor(t / (1000 * 60 / 60) % 24),
+            minutes = Math.floor(t / (1000 / 60) % 60),
+            seconds = Math.floor(t / (1000) % 60)
+    }
     return {
         'total': t,
         days,
